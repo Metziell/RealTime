cf61d6c06762a85f5a0c3e292e3cb85433aadaa2
nktae <nkt2823@gmail.com>
2023-07-01 16:19:27 +0300

    Update to 1.20.7

--- src/RealTime/CustomAI/RealTimeTouristAI.cs
+++ src/RealTime/CustomAI/RealTimeTouristAI.cs
@@ -1,10 +1,11 @@
-﻿// <copyright file="RealTimeTouristAI.cs" company="dymanoid">
+// <copyright file="RealTimeTouristAI.cs" company="dymanoid">
 // Copyright (c) dymanoid. All rights reserved.
 // </copyright>
 
 namespace RealTime.CustomAI
 {
     using System;
+    using ColossalFramework;
     using RealTime.Config;
     using RealTime.Events;
     using RealTime.GameConnection;
@@ -254,7 +255,7 @@ namespace RealTime.CustomAI
 
         private void FindRandomVisitPlace(TAI instance, uint citizenId, ref TCitizen citizen, int doNothingProbability, ushort currentBuilding)
         {
-            var target = (TouristTarget)touristAI.GetRandomTargetType(instance, doNothingProbability);
+            var target = (TouristTarget)touristAI.GetRandomTargetType(instance, doNothingProbability, ref citizen);
             target = AdjustTargetToTimeAndWeather(ref citizen, target);
 
             switch (target)
--- src/RealTime/GameConnection/TouristAIConnection.cs
+++ src/RealTime/GameConnection/TouristAIConnection.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="TouristAIConnection.cs" company="dymanoid">
+// <copyright file="TouristAIConnection.cs" company="dymanoid">
 // Copyright (c) dymanoid. All rights reserved.
 // </copyright>
 
@@ -52,7 +52,8 @@ namespace RealTime.GameConnection
         /// <param name="instance">The AI instance the method is called on.</param>
         /// <param name="doNothingProbability">A value that specified a probability that the citizen will do nothing.</param>
         /// <returns>A value specifying the citizen's next action: 0 for idle, 1 for leaving the city, 2 for shopping, 3 for entertainment.</returns>
-        public delegate int GetRandomTargetTypeDelegate(TAI instance, int doNothingProbability);
+        /// <param name="data">The refernece to the citizen struct for this instance.</param>
+        public delegate TouristAI.Target GetRandomTargetTypeDelegate(TAI instance, int doNothingProbability, ref TCitizen data);
 
         /// <summary>
         /// Represents the method that corresponds to the AI's original <c>GetLeavingReason</c> method.
